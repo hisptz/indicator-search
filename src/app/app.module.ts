13949bc02c6691f4e59a96f2b189ce6bb24f37f0
josephatJ <josephatjulius24@gmail.com>
2020-01-26 12:06:50 +0300

    (featuree): Create environment for supporting different download templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
+import { environment } from "../environments/environment";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { environment } from '../environments/environment';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
-import { effects } from './store/app.effects';
-import { metaReducers, reducers } from './store/app.reducers';
+import { effects } from "./store/app.effects";
+import { metaReducers, reducers } from "./store/app.reducers";
 
 // Modules
-import {NgxDhis2MenuModule} from '@hisptz/ngx-dhis2-menu';
-import {NgxDhis2DictionaryModule} from '@iapps/ngx-dhis2-dictionary';
-import { HttpClientModule } from '@angular/common/http';
-import { TreeModule } from 'angular-tree-component';
+import { NgxDhis2MenuModule } from "@hisptz/ngx-dhis2-menu";
+import { NgxDhis2DictionaryModule } from "@iapps/ngx-dhis2-dictionary";
+import { HttpClientModule } from "@angular/common/http";
+import { TreeModule } from "angular-tree-component";
+import { ExportService } from "./services/export.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -35,10 +32,10 @@ import { TreeModule } from 'angular-tree-component';
     TreeModule.forRoot(),
     NgxDhis2MenuModule,
     NgxDhis2DictionaryModule,
-      /**
+    /**
      * Reducers
      */
-    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreModule.forRoot(reducers, { metaReducers }),
 
     /**
      * Effects
@@ -55,7 +52,7 @@ import { TreeModule } from 'angular-tree-component';
      */
     !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
-  providers: [],
+  providers: [ExportService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,107 +1,181 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import * as _ from 'lodash';
-import { NgxDhis2HttpClientService } from '@iapps/ngx-dhis2-http-client';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import * as _ from "lodash";
+import { NgxDhis2HttpClientService } from "@iapps/ngx-dhis2-http-client";
+import { ExportService } from "src/app/services/export.service";
+import { DatePipe } from "@angular/common";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
   selectedItem: string;
   metadataIdentifiers: any;
   metadataIdentifiersArr: any[] = [];
   systemInfo: any;
-  constructor(private router: Router, private route: ActivatedRoute, private httpClient: NgxDhis2HttpClientService) {}
+  loadedMetadataInfo: any;
+  isViewAllIndicatorDownloadTemplateSet: Boolean = false;
+  html: any;
+  isprintSet: Boolean = false;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private httpClient: NgxDhis2HttpClientService,
+    private exportService: ExportService,
+    private datePipe: DatePipe
+  ) {}
 
   ngOnInit() {
-    this.httpClient.get('system/info').subscribe((systemSettings) => {
+    this.httpClient.get("system/info").subscribe(systemSettings => {
       this.systemInfo = systemSettings;
-    })
+    });
     this.route.params.forEach((params: Params) => {
-      if (params['selected'] != undefined) {
-        if (params['selected'] == 'all' && !params['ids']) {
+      if (params["selected"] != undefined) {
+        if (params["selected"] == "all" && !params["ids"]) {
           this.metadataIdentifiers = [];
-          this.selectedItem = params['selected']
-          this.router.navigate(['dictionary/all'])
+          this.selectedItem = params["selected"];
+          this.router.navigate(["dictionary/all"]);
         } else {
-          this.selectedItem = params['selected'];
+          this.selectedItem = params["selected"];
           let identifiers = [];
-          params['ids'].split(',').forEach((param) => {
+          params["ids"].split(",").forEach(param => {
             identifiers.push(param);
-          })
-          if (this.selectedItem != 'all') {
+          });
+          if (this.selectedItem != "all") {
             identifiers.push(this.selectedItem);
           }
           this.metadataIdentifiers = _.uniq(identifiers);
-          this.router.navigate(['dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + this.selectedItem])
+          this.router.navigate([
+            "dictionary/" +
+              _.uniq(identifiers).join(",") +
+              "/selected/" +
+              this.selectedItem
+          ]);
         }
       } else {
         this.metadataIdentifiers = this.metadataIdentifiersArr;
-        this.selectedItem = 'all';
-        this.router.navigate(['dictionary/' + _.uniq(this.metadataIdentifiers).join(',') + '/selected/' + this.selectedItem])
+        this.selectedItem = "all";
+        this.router.navigate([
+          "dictionary/" +
+            _.uniq(this.metadataIdentifiers).join(",") +
+            "/selected/" +
+            this.selectedItem
+        ]);
       }
-    })
+    });
   }
 
   dictionaryItemId(listOfItemsObj) {
-    if (listOfItemsObj.selected == 'all') {
-      this.metadataIdentifiers = listOfItemsObj['otherSelectedIds'];
+    if (listOfItemsObj.selected == "all") {
+      this.metadataIdentifiers = listOfItemsObj["otherSelectedIds"];
       if (this.metadataIdentifiers.length > 0) {
         let identifiers = [];
-        _.map(this.metadataIdentifiers, (identifier) =>{
-          if (identifier != 'all') {
-            identifiers.push(identifier)
+        _.map(this.metadataIdentifiers, identifier => {
+          if (identifier != "all") {
+            identifiers.push(identifier);
           }
-        })
+        });
         this.metadataIdentifiers = identifiers;
         this.selectedItem = listOfItemsObj.selected;
-        this.router.navigate(['dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + listOfItemsObj.selected])
+        this.router.navigate([
+          "dictionary/" +
+            _.uniq(identifiers).join(",") +
+            "/selected/" +
+            listOfItemsObj.selected
+        ]);
       } else {
-        this.router.navigate(['dictionary/all'])
+        this.router.navigate(["dictionary/all"]);
       }
     } else {
       let identifiers = [];
-      listOfItemsObj['otherSelectedIds'].forEach((identifier) => {
-        if (identifier != 'all') {
+      listOfItemsObj["otherSelectedIds"].forEach(identifier => {
+        if (identifier != "all") {
           identifiers.push(identifier);
         }
-      })
-      if (_.indexOf(identifiers,listOfItemsObj.selected) < 0) {
-        identifiers.push(listOfItemsObj.selected)
+      });
+      if (_.indexOf(identifiers, listOfItemsObj.selected) < 0) {
+        identifiers.push(listOfItemsObj.selected);
       }
       this.metadataIdentifiers = _.uniq(identifiers);
       this.selectedItem = listOfItemsObj.selected;
-      this.router.navigate(['dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + listOfItemsObj.selected])
+      this.router.navigate([
+        "dictionary/" +
+          _.uniq(identifiers).join(",") +
+          "/selected/" +
+          listOfItemsObj.selected
+      ]);
     }
   }
 
   getSharedLink(identifiers) {
     if (this.systemInfo) {
-      let selBox = document.createElement('textarea');
-      selBox.style.position = 'fixed';
-      selBox.style.left = '0';
-      selBox.style.top = '0';
-      selBox.style.opacity = '0';
+      let selBox = document.createElement("textarea");
+      selBox.style.position = "fixed";
+      selBox.style.left = "0";
+      selBox.style.top = "0";
+      selBox.style.opacity = "0";
       if (identifiers.length > 0) {
         if (this.systemInfo.instanceBaseUrl) {
-          selBox.value = this.systemInfo.instanceBaseUrl + '/api/apps/Indicator-Dictionary/index.html#/dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + this.selectedItem;
+          selBox.value =
+            this.systemInfo.instanceBaseUrl +
+            "/api/apps/Indicator-Dictionary/index.html#/dictionary/" +
+            _.uniq(identifiers).join(",") +
+            "/selected/" +
+            this.selectedItem;
         } else {
-          selBox.value = this.systemInfo.contextPath + '/api/apps/Indicator-Dictionary/index.html#/dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + this.selectedItem;
+          selBox.value =
+            this.systemInfo.contextPath +
+            "/api/apps/Indicator-Dictionary/index.html#/dictionary/" +
+            _.uniq(identifiers).join(",") +
+            "/selected/" +
+            this.selectedItem;
         }
       } else {
-        selBox.value = this.systemInfo.contextPath + '/api/apps/Indicator-Dictionary/index.html#/dictionary/all';
+        selBox.value =
+          this.systemInfo.contextPath +
+          "/api/apps/Indicator-Dictionary/index.html#/dictionary/all";
       }
       document.body.appendChild(selBox);
       selBox.focus();
       selBox.select();
-      document.execCommand('copy');
+      document.execCommand("copy");
       document.body.removeChild(selBox);
-      let messagePart = document.getElementById('copied-message')
-      setTimeout(function(){ messagePart.style.display = 'inline-block'; messagePart.style.marginBottom = '-20px';}, 100);
-      setTimeout(function(){ messagePart.style.display = 'none'; }, 1000);
+      let messagePart = document.getElementById("copied-message");
+      setTimeout(function() {
+        messagePart.style.display = "inline-block";
+        messagePart.style.marginBottom = "-20px";
+      }, 100);
+      setTimeout(function() {
+        messagePart.style.display = "none";
+      }, 1000);
     }
+  }
+
+  metadataInfo(loadedMetadata) {
+    this.loadedMetadataInfo = loadedMetadata;
+  }
+
+  toggleDownloadTemplateAndDictionaryList() {
+    this.isViewAllIndicatorDownloadTemplateSet = !this
+      .isViewAllIndicatorDownloadTemplateSet;
+  }
+
+  exportMetadataInformation(id) {
+    this.html = document.getElementById(id).outerHTML;
+    let theDate = new Date();
+    this.datePipe.transform(theDate, "yyyy-MM-dd");
+    this.exportService.exportXLS(
+      "all_metadata_template_generated_on_" +
+        this.datePipe.transform(theDate, "yyyy-MM-dd"),
+      this.html
+    );
+  }
+
+  printPDF() {
+    setTimeout(function() {
+      window.print();
+    }, 500);
   }
 }
